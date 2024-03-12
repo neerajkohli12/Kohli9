a4e7cad310b4d5b8e81f310d0bd828ceca17577a
Rohaniscoder <109896047+Rohaniscoder@users.noreply.github.com>
2024-03-12 18:30:47 +0530

    Update info.py

--- info.py
+++ info.py
@@ -45,8 +45,8 @@ SUPPORT_CHAT_ID = int(support_chat_id) if support_chat_id and id_pattern.search(
 NO_RESULTS_MSG = bool(environ.get("NO_RESULTS_MSG", False))
 
 # MongoDB information
-DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://UrlUploader:z8jpH2g4F3pvfUzw@cluster0.7xweeyy.mongodb.net/?retryWrites=true&w=majority")
-DATABASE_NAME = environ.get('DATABASE_NAME', "UrlUploader")
+DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://nrmoviebot:LjwCViSI3x5KiAwM@cluster0.sshhuya.mongodb.net/?retryWrites=true&w=majority")
+DATABASE_NAME = environ.get('DATABASE_NAME', "nrmoviebot")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
 
 # Others
